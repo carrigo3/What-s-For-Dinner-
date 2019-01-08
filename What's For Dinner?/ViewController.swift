48c27bc491e4ab4a9e059ac36b91c0863934fde2
Cam Arrigo <carrigo3@gmail.com>
2019-01-08 11:03:02 -0500

    Completed What's For Dinner App

--- What's For Dinner?/ViewController.swift
+++ What's For Dinner?/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var foodPic: UIImageView!
+    @IBOutlet weak var foodLabel: UILabel!
+    var index = -1
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func whatsForDinner(_ sender: UIButton) {
+        var options = ["Cheese Pizza", "Burrito", "Chicken Nuggets", "Soup", "Salad", "Chicken Parmesan"]
+        var newIndex: Int
+        repeat{
+            newIndex = Int.random(in: 0..<options.count)
+        } while index == newIndex
+            index = newIndex
+            foodLabel.text = options[index]
+            foodPic.image = UIImage(named:"image\(index)")
+        
+    }
+    
+    
 
 }
 
